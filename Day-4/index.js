d9d6140ca0923734f1a9992c6bd21d347cd31712
Mihir2003 <89642436+Mihir2003@users.noreply.github.com>
2026-06-13 23:40:18 +0530

    updates for day 4

---
+++ Day-4/index.js
@@ -0,0 +1,91 @@
+// Array 
+
+var students = [
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+    "rohit",
+    "virat",
+    "mihir",
+
+];
+console.log(students);
+console.log(students[1]);
+console.log(students[2]);
+console.log(students[3]);
+console.log(students.length);
+console.log(students[students.length - 1]);
+
+
+console.log((students.lenghth - 1) / 2);
+console.log(students[(students.length - 1) / 2]);
+students[(students.length - 1) / 2] = "mihir";
+console.log(students[(students.length -1) / 2]);
+
+
+
+
+students[90] = "Hello";
+console.log(students[70]);
+console.log(students);
+console.log(students[40]);
+
+
